a92e0b9afa7a6d07c37330bc152bdced203d4f21
rahulpanwar-tm <rahul.panwar@teramatrix.in>
2020-02-28 18:50:59 +0530

    update count case in deployement script

--- jenkins/ol_core_v2/backup_script/mysql-import.sh
+++ jenkins/ol_core_v2/backup_script/mysql-import.sh
@@ -1,1 +1,2 @@
- mysql -u root --host 192.168.1.34 --port 3306 -pTtpl@123 rp < /home/ttpl/jenkins_backup_script/28Feb2020/rp-28Feb2020.sql
+VAR=$(ls -tr|tail -1)
+mysql -u root --host 192.168.1.34 --port 3306 -pTtpl@123 rp < /home/ttpl/jenkins_backup_script/$VAR/rp-$VAR.sql
