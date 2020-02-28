e12530fcd470d8c64c9ed37d069a60aa384ae3d3
rahulpanwar-tm <rahul.panwar@teramatrix.in>
2020-02-28 19:09:12 +0530

    update count case in deployement script

--- jenkins/ol_core_v2/backup_script/mysql-import-remote.sh
+++ jenkins/ol_core_v2/backup_script/mysql-import-remote.sh
@@ -1,3 +1,3 @@
-sshpass -p "ttpl@123"  ssh ttpl@192.168.1.34  'gzip -d /home/ttpl/jenkins_backup_script/28Feb2020/rp-28Feb2020.sql.gz'
+sshpass -p "ttpl@123"  ssh ttpl@192.168.1.34  'gzip -d /home/ttpl/jenkins_backup_script/$(ls -tr|tail -1)/rp-$(ls -tr|tail -1).sql.gz'
 sshpass -p "ttpl@123"  ssh ttpl@192.168.1.34  'bash /home/ttpl/jenkins_backup_script/mysql-import.sh'
 
