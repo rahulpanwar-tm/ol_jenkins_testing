a406519fcee35fd6375edfd92d31e2bc0ec43b55
rahulpanwar-tm <rahul.panwar@teramatrix.in>
2020-02-28 15:25:23 +0530

    update count case in deployement script

--- jenkins/ol_core_v2/olcore_v2_schema_2020_01_23.sql
+++
@@ -1,4 +0,0 @@
-USE gmonstar_orchestration;
-
-update inherit_api_input_parameters set  client_parameter_value ="application/json;charset=UTF-8" 
-where client_parameter_value="application/json" and is_header=1;
