031ec910309f84e1402611dc08d5b3312def22af
rahulpanwar-tm <rahul.panwar@teramatrix.in>
2020-03-17 13:30:07 +0530

    update all import

--- jenkins/ol_core_v2/backup_script/mysql-import-remote.sh
+++ jenkins/ol_core_v2/backup_script/mysql-import-remote.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+#!/usr/bin/env bash
 # sshpass -p "ttpl@123"  ssh ttpl@192.168.1.34  'gzip -d /home/ttpl/jenkins_backup_script/$(ls -tr|tail -1)/rp-$(ls -tr|tail -1).sql.gz'
 Execution_command='$3mysql-import.sh $1 $2'
+echo $Execution_command
 sshpass -p "ttpl@123"  ssh ttpl@192.168.1.34  'bash $Execution_command'
