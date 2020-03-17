2173b02e8ea9809a1ce01758317081ca3e08d689
rahulpanwar-tm <rahul.panwar@teramatrix.in>
2020-03-17 11:15:46 +0530

    update all import

--- jenkins/ol_core_v2/backup_script/mysql-backup.sh
+++ jenkins/ol_core_v2/backup_script/mysql-backup.sh
@@ -3,9 +3,8 @@
 ################################################################
 ##
 ##   MySQL Database Backup Script 
-##   Written By: Rahul Kumar
-##   URL: https://tecadmin.net/bash-script-mysql-database-backup/
-##   Last Update: Jan 05, 2019
+##   Written By: Rahul Panwar
+##   Last Update: Jan 05, 2020
 ##
 ################################################################
  
@@ -20,9 +19,18 @@ MYSQL_HOST=$2
 MYSQL_PORT=$3
 MYSQL_USER=$4
 MYSQL_PASSWORD=$5
-DATABASE_NAME=$6
+#DATABASE_NAME=$6
 BACKUP_RETAIN_DAYS=30   ## Number of days to keep local backup copy
+declare -a arr=("rp" "rp_tp")
+rm -rf ${DB_BACKUP_PATH}/${TODAY}
+mkdir -p ${DB_BACKUP_PATH}/${TODAY}
 
+
+##################For loop for database name ###################
+for DATABASE_NAME  in "${arr[@]}"
+do
+#TODAY=`date +"%d_%m_%Y_%H_%M_%S"`
+echo ${DATABASE_NAME}
 ######################Checking database exist or not ##########################
 RESULT=`mysqlshow  -u root -h $MYSQL_HOST -p$MYSQL_PASSWORD $DATABASE_NAME | grep -v Wildcard | grep -o $DATABASE_NAME`
 echo "$RESULT"
@@ -65,3 +73,4 @@ else
 mysql -u$MYSQL_USER --port $MYSQL_PORT  -p$MYSQL_PASSWORD  -c -h $MYSQL_HOST -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME; DROP TABLE IF EXISTS $DATABASE_NAME.version_status; CREATE TABLE IF NOT EXISTS $DATABASE_NAME.version_status (id int(11) NOT NULL AUTO_INCREMENT,project_name varchar(50) DEFAULT NULL,model_name varchar(50) DEFAULT NULL,script_name varchar(50) DEFAULT NULL,installation_date int(11) DEFAULT NULL,script_date int(11) DEFAULT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8; INSERT INTO $DATABASE_NAME.version_status (id,project_name,model_name,script_name,installation_date,script_date) VALUES (423424341, 'olCore', 'olcore', 'olcore_v2_schema_2010_08_21.sql', 1582809983, 1566345600);"
 fi
 ### End of script ####
+done
--- jenkins/ol_core_v2/backup_script/mysql-import.sh
+++ jenkins/ol_core_v2/backup_script/mysql-import.sh
@@ -1,4 +1,13 @@
 cd /home/ttpl/jenkins_backup_script/
 VAR=$(ls -tr|tail -1)
-gzip -d /home/ttpl/jenkins_backup_script/$VAR/rp-$VAR.sql.gz
-mysql -u root --host 192.168.1.34 --port 3306 -pTtpl@123 rp < /home/ttpl/jenkins_backup_script/$VAR/rp-$VAR.sql
+## declare an array variable
+declare -a arr=("rp" "rp_tp")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   echo "$i"
+   # or do whatever with individual element of the array
+gzip -d /home/ttpl/jenkins_backup_script/$VAR/$i-$VAR.sql.gz
+mysql -u root --host 192.168.1.34 --port 3306 -pTtpl@123 $i < /home/ttpl/jenkins_backup_script/$VAR/$i-$VAR.sql
+done
